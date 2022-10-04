6e3ec5afd40502a19117691ed19ecc3579dbceb8
RSpeckles <rchenspeckman@gmail.com>
2022-10-03 18:10:58 -0700

    Completed question 1

--- q1.py
+++ q1.py
@@ -2,7 +2,28 @@
 # ******************************
 # Make your Code
 # ******************************
+nums = []
+count = 0
+cluster = 0
 
+while len(nums) < 10:
+        try:
+            nums.append(int(input("Enter a number: ")))
+        except ValueError:
+            print("Must be a numeric value")
+
+for i in range(0,10):
+    if nums[i] % 2 == 0:
+        count = count+1
+    elif count > 1:
+        cluster = cluster+1
+        count = 0
+    else:
+        count = 0
+if count > 1:
+    cluster = cluster+1
+
+print(cluster)
 
 # Requirement
 # No need to use list
